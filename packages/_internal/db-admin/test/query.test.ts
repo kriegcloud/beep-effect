759cd27ef45f1b221910fea8403430b89524aa42
elpresidank <benjamin@krieg.cloud>
2025-09-15 07:27:32 -0500

    got integration tests working with pg testcontainer in @beep/db-admin. F yes...

--- packages/_internal/db-admin/test/query.test.ts
+++ packages/_internal/db-admin/test/query.test.ts
@@ -2,13 +2,11 @@ import * as Schema from "@beep/db-admin/schema";
 import * as PgDrizzle from "@effect/sql-drizzle/Pg";
 import { expect, it } from "@effect/vitest";
 import * as Effect from "effect/Effect";
-import * as Layer from "effect/Layer";
 import { PgContainer } from "./pg-container";
 
-const i = Layer.mergeAll(PgContainer.Live);
-it.layer(i, { timeout: "30 seconds" })("StylesRepo", (it) => {
+it.layer(PgContainer.Live, { timeout: "30 seconds" })("mocked drizzle db", (it) => {
   it.effect(
-    "should create a style",
+    "mocked drizzle db should work",
     Effect.fnUntraced(function* () {
       const db = yield* PgDrizzle.make<typeof Schema>({
         schema: Schema,
