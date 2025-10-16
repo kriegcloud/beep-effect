34eebd44a16b08ee73a186c743ba025b0ce5bcf8
elpresidank <benjamin@krieg.cloud>
2025-10-15 19:01:34 -0500

    fixed type annotation issue cause .github actions pipeline to fail

--- packages/iam/infra/src/adapters/better-auth/plugins/one-time-token/one-time-token.plugin.ts
+++ packages/iam/infra/src/adapters/better-auth/plugins/one-time-token/one-time-token.plugin.ts
@@ -3,4 +3,4 @@ import * as Effect from "effect/Effect";
 
 export type OneTimeTokenPluginEffect = Effect.Effect<ReturnType<typeof oneTimeToken>, never, never>;
 export type OneTimeTokenPlugin = Effect.Effect.Success<OneTimeTokenPluginEffect>;
-export const oneTimeTokenPlugin = Effect.succeed(oneTimeToken());
+export const oneTimeTokenPlugin: OneTimeTokenPluginEffect = Effect.succeed(oneTimeToken());
