9e863c1817181356b1fa620a56f52eb247e8f28c
elpresidank <benjamin@krieg.cloud>
2026-01-14 18:06:53 -0600

    updated deps

--- packages/iam/client/src/adapters/better-auth/client.ts
+++ packages/iam/client/src/adapters/better-auth/client.ts
@@ -6,16 +6,16 @@ import {passkeyClient} from "@better-auth/passkey/client";
 import {ssoClient} from "@better-auth/sso/client";
 import {stripeClient} from "@better-auth/stripe/client";
 import {
-  // adminClient,
+  adminClient,
   anonymousClient,
-  // apiKeyClient,
-  // deviceAuthorizationClient,
-  // genericOAuthClient,
-  // jwtClient,
+  apiKeyClient,
+  deviceAuthorizationClient,
+  genericOAuthClient,
+  jwtClient,
   inferOrgAdditionalFields,
   lastLoginMethodClient,
-  // multiSessionClient,
-  // oidcClient,
+  multiSessionClient,
+  oidcClient,
   oneTapClient,
   oneTimeTokenClient,
   organizationClient,
@@ -24,32 +24,24 @@ import {
   usernameClient,
 } from "better-auth/client/plugins";
 import {createAuthClient} from "better-auth/react";
-import {$IamClientId} from "@beep/identity/packages";
-import * as A from "effect/Array";
-import * as O from "effect/Option";
-import * as Effect from "effect/Effect";
-import * as F from "effect/Function";
-import * as S from "effect/Schema";
-import * as Struct from "effect/Struct";
+import * as Duration from "effect/Duration";
 import * as P from "effect/Predicate";
-import type * as Cause from "effect/Cause";
-
 export const client = createAuthClient({
   baseURL: clientEnv.authUrl,
   basePath: clientEnv.authPath,
   plugins: [
-    // inferAdditionalFields<Auth.Auth>(),
-    // adminClient(),
+    adminClient(),
     anonymousClient(),
-    // jwtClient(),
-    // apiKeyClient(),
-    // genericOAuthClient(),
-    // multiSessionClient(),
-    // oidcClient(),
+
+    jwtClient(),
+    apiKeyClient(),
+    genericOAuthClient(),
+    multiSessionClient(),
+    oidcClient(),
     oneTapClient({
       clientId: clientEnv.googleClientId,
       promptOptions: {
-        baseDelay: 1000, // Base delay in ms (default: 1000)
+        baseDelay: Duration.toMillis(Duration.seconds(1)), // Base delay in ms (default: 1000)
         maxAttempts: 5, // Maximum number of attempts before triggering onPromptNotification (default: 5)
       },
       context: "signin",
@@ -68,12 +60,11 @@ export const client = createAuthClient({
     phoneNumberClient(),
     siweClient(),
     ssoClient(),
-
     usernameClient(),
     stripeClient({
       subscription: true,
     }),
-    // deviceAuthorizationClient(),
+    deviceAuthorizationClient(),
     lastLoginMethodClient(),
   ],
 } satisfies BetterAuthClientOptions);
@@ -82,27 +73,5 @@ export const {$store, signIn, signUp} = client;
 
 $store.listen("$sessionSignal", asyncNoOp);
 
-
-const $I = $IamClientId.create("adapters/better-auth/client");
-
-export const isClientMethod = (u: unknown): u is ((i: Parameters<typeof client.signIn[keyof typeof client.signIn]>[0]) => Awaited<ReturnType<typeof client.signIn[keyof typeof client.signIn]>>) =>
-  P.isFunction(u);
-const clientMethodKeys = S.keyof(client.signIn);
-
-
 export const isClientMethodKey = (u: unknown): u is keyof typeof client.signIn =>
   P.isString(u);
-
-
-
-
-
-// F.pipe(
-//       [k, v],
-//       O.liftPredicate(P.tuple(
-//         S.is(clientMethodKeys),
-//         isClientMethod,
-//       )),
-//       Effect.flatMap(([k, v]) => Effect.tryPromise(() => client[k](...v))),
-//
-//     )
