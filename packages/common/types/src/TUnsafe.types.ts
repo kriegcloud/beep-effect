362518c41357828c18d88c2db7c471769930cffa
elpresidank <benjamin@krieg.cloud>
2026-02-25 06:05:40 -0600

    fix(docs): aggregate nested package docgen output

--- packages/common/types/src/TUnsafe.types.ts
+++ packages/common/types/src/TUnsafe.types.ts
@@ -5,7 +5,7 @@
  *
  * import { Unsafe } from "@beep/types";
  *
- * export const youShouldFeelShame = (yeet: Unsafe.Any) => console.log(yeet);
+ * export const youShouldFeelShame = (beepBopDumbass: Unsafe.Any) => console.log(beepBopDumbass);
  *
  * @module @beep/types/Unsafe.types
  * @since 0.0.0
@@ -13,13 +13,13 @@
 
 /**
  *
- * Hopefully the one any used in the repo.
+ * Hopefully the only any used in the repo.
  *
  * @example
  *
  * import { Unsafe } from "@beep/types";
  *
- * export const youShouldFeelShame = (yeet: Unsafe.Any) => console.log(yeet);
+ * export const youShouldFeelShame = (beepBopDumbass: Unsafe.Any) => console.log(beepBopDumbass);
  *
  * @category Unsafe
  * @since 0.0.0
