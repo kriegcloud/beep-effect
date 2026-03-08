181bb5b9f7f4149b4a61f7b9bb6eecee2e910d3b
elpresidank <benjamin@krieg.cloud>
2026-03-07 22:54:21 -0600

    Squashed '.repos/semantic-web/jsonld-streaming-parser.js/' content from commit 4f22c20a99
    git-subtree-dir: .repos/semantic-web/jsonld-streaming-parser.js
    git-subtree-split: 4f22c20a9921427798bb0096f8f9b5f5a8a47857

---
+++ test/ContextTree-test.ts
@@ -0,0 +1,78 @@
+import {ContextTree} from "../lib/ContextTree";
+import {JsonLdContextNormalized} from "jsonld-context-parser/lib/JsonLdContextNormalized";
+
+describe('ContextTree', () => {
+
+  describe('an empty instance', () => {
+    let tree: ContextTree;
+
+    beforeEach(() => {
+      tree = new ContextTree();
+    });
+
+    it('should have no contexts', async () => {
+      expect(tree.getContext([])).toBeFalsy();
+      expect(tree.getContext(['a'])).toBeFalsy();
+      expect(tree.getContext(['a', 'b'])).toBeFalsy();
+    });
+
+    it('should should set a context at depth 1', async () => {
+      const c = Promise.resolve(new JsonLdContextNormalized({}));
+      tree.setContext(['a'], c);
+      expect(await tree.getContext(['a'])).toEqual({ context: await c, depth: 1 });
+      expect(await tree.getContext(['a', 'b'])).toEqual({ context: await c, depth: 1 });
+    });
+
+    it('should should set a context at depth 1 for an undefined key', async () => {
+      const c = Promise.resolve(new JsonLdContextNormalized({ a: 'b' }));
+      tree.setContext([undefined], c);
+      expect(await tree.getContext([<any>undefined])).toEqual({ context: await c, depth: 1 });
+      expect(await tree.getContext([<any>undefined, 'b'])).toEqual({ context: await c, depth: 1 });
+    });
+
+    it('should should set a context at depth 2', async () => {
+      const c = Promise.resolve(new JsonLdContextNormalized({}));
+      tree.setContext(['a', 'b'], c);
+      expect(tree.getContext(['a'])).toBeFalsy();
+      expect(await tree.getContext(['a', 'b'])).toEqual({ context: await c, depth: 2 });
+    });
+
+    it('should allow branched context setting', async () => {
+      const c1 = Promise.resolve(new JsonLdContextNormalized({}));
+      const c2 = Promise.resolve(new JsonLdContextNormalized({}));
+      tree.setContext(['a', 'b'], c1);
+      tree.setContext(['a', 'c'], c2);
+
+      expect(tree.getContext(['a'])).toBeFalsy();
+      expect(await tree.getContext(['a', 'b'])).toEqual({ context: await c1, depth: 2 });
+      expect(await tree.getContext(['a', 'c'])).toEqual({ context: await c2, depth: 2 });
+    });
+
+    it('should not allow overriding contexts', async () => {
+      const c1 = Promise.resolve(new JsonLdContextNormalized({}));
+      const c2 = Promise.resolve(new JsonLdContextNormalized({}));
+      tree.setContext(['a', 'b'], c1);
+      tree.setContext(['a', 'b'], c2);
+
+      expect(tree.getContext(['a'])).toBeFalsy();
+      expect(await tree.getContext(['a', 'b'])).toEqual({ context: await c2, depth: 2 });
+    });
+  });
+
+  describe('an instance with a root context', () => {
+    let tree: any;
+    const root = Promise.resolve(new JsonLdContextNormalized({}));
+
+    beforeEach(() => {
+      tree = new ContextTree();
+      tree.setContext(['a', 'b'], root);
+    });
+
+    it('should return the root', async () => {
+      expect(tree.getContext([])).toBeFalsy();
+      expect(tree.getContext(['a'])).toBeFalsy();
+      expect(await tree.getContext(['a', 'b'])).toEqual({ context: await root, depth: 2 });
+      expect(await tree.getContext(['a', 'b', 'c'])).toEqual({ context: await root, depth: 2 });
+    });
+  });
+});
