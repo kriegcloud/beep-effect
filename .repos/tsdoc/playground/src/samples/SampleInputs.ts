3629fe67c66150f9cd9645e717fc2a7dc274d788
elpresidank <benjamin@krieg.cloud>
2026-03-08 07:33:32 -0500

    fix(desktop): restore tauri csp

--- .repos/tsdoc/playground/src/samples/SampleInputs.ts
+++
@@ -1,5 +0,0 @@
-export namespace SampleInputs {
-  export const advanced: string = require('./advancedSample.ts');
-  export const basic: string = require('./basicSample.ts');
-  export const hyperlink: string = require('./hyperlinkSample.ts');
-}
