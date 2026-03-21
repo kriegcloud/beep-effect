7c2e091116a5c2677df250a416c491e7f6da1eeb
elpresidank <benjamin@krieg.cloud>
2026-03-21 01:24:16 -0500

    Squashed '.repos/drizzle-orm/' content from commit 7eb39f01c0
    git-subtree-dir: .repos/drizzle-orm
    git-subtree-split: 7eb39f01c0658e34c62233e34b2ebfb0519645f8

---
+++ drizzle-kit/src/dialects/sqlite/serializer.ts
@@ -0,0 +1,71 @@
+import type { CasingType } from 'src/cli/validations/common';
+import { sqliteSchemaError } from '../../cli/views';
+import { findLeafSnapshotIds, prepareFilenames } from '../../utils/utils-node';
+import type { SQLiteDDL } from './ddl';
+import { createDDL, interimToDDL } from './ddl';
+import { fromDrizzleSchema, prepareFromSchemaFiles } from './drizzle';
+import type { SqliteSnapshot } from './snapshot';
+import { drySqliteSnapshot, snapshotValidator } from './snapshot';
+
+export const prepareSqliteSnapshot = async (
+	snapshots: string[],
+	schemaPath: string | string[],
+	casing: CasingType | undefined,
+): Promise<{
+	ddlPrev: SQLiteDDL;
+	ddlCur: SQLiteDDL;
+	snapshot: SqliteSnapshot;
+	snapshotPrev: SqliteSnapshot;
+	custom: SqliteSnapshot;
+}> => {
+	const { readFileSync } = await import('fs');
+	const { randomUUID } = await import('crypto');
+	const prevSnapshot = snapshots.length === 0
+		? drySqliteSnapshot
+		: snapshotValidator.strict(
+			JSON.parse(readFileSync(snapshots[snapshots.length - 1]).toString()),
+		);
+
+	const ddlPrev = createDDL();
+	for (const entry of prevSnapshot.ddl) {
+		ddlPrev.entities.push(entry);
+	}
+	const filenames = prepareFilenames(schemaPath);
+
+	const { tables, views } = await prepareFromSchemaFiles(filenames);
+	const interim = fromDrizzleSchema(tables, views, casing);
+
+	const { ddl: ddlCur, errors } = interimToDDL(interim);
+
+	if (errors.length > 0) {
+		console.log(errors.map((it) => sqliteSchemaError(it)).join('\n\n'));
+		process.exit();
+	}
+
+	const id = randomUUID();
+	const prevIds = snapshots.length === 0 ? [prevSnapshot.id] : findLeafSnapshotIds(snapshots);
+
+	const snapshot = {
+		version: '7',
+		dialect: 'sqlite',
+		id,
+		prevIds,
+		ddl: ddlCur.entities.list(),
+		renames: [],
+	} satisfies SqliteSnapshot;
+
+	const {
+		id: _ignoredId,
+		prevIds: _ignoredPrevIds,
+		...prevRest
+	} = prevSnapshot;
+
+	// that's for custom migrations, when we need new IDs, but old snapshot
+	const custom: SqliteSnapshot = {
+		id,
+		prevIds,
+		...prevRest,
+	};
+
+	return { ddlPrev, ddlCur, snapshot, snapshotPrev: prevSnapshot, custom };
+};
