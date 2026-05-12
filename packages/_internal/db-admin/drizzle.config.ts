9e2d22bb234462809204c47121b947f479cf36d4
elpresidank <benjamin@krieg.cloud>
2026-05-12 18:22:40 -0500

    fixed invalid drizzle config

--- packages/_internal/db-admin/drizzle.config.ts
+++ packages/_internal/db-admin/drizzle.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  casing: "camelCase",
   dbCredentials: {
     url: process.env.BEEP_TEST_DATABASE_URL ?? "postgres://postgres:postgres@127.0.0.1:5432/postgres",
   },
   dialect: "postgresql",
+  introspect: {
+    casing: "camel",
+  },
   out: "./drizzle",
   schema: "./src/schema.ts",
 });
