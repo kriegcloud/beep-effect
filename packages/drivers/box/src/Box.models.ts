ff9b993d1f263e0b64334d14c4906275aa918e1d
elpresidank <benjamin@krieg.cloud>
2026-05-30 02:16:05 -0500

    saving

--- packages/drivers/box/src/Box.models.ts
+++ packages/drivers/box/src/Box.models.ts
@@ -4,9 +4,9 @@
  * @packageDocumentation
  * @since 0.0.0
  */
-import { $BoxId } from "@beep/identity";
-import * as S from "effect/Schema";
-
-const $I = $BoxId.create("Box.models");
+// import { $BoxId } from "@beep/identity";
+// import * as S from "effect/Schema";
+//
+// const $I = $BoxId.create("Box.models");
 
 
