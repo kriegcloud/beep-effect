93713c12dc5ffa3c4a2d05a5fd4f78183e7ae0d2
elpresidank <benjamin@krieg.cloud>
2026-06-29 04:21:11 -0500

    saving

--- packages/drivers/govinfo/src/domain/contracts/Search/Search.contract.ts
+++ packages/drivers/govinfo/src/domain/contracts/Search/Search.contract.ts
@@ -4,13 +4,13 @@
  * @packageDocumentation
  * @since 0.0.0
  */
-import {$GovinfoId} from "@beep/identity";
-import {LiteralKit, TaggedErrorClass} from "@beep/schema";
+import { $GovinfoId } from "@beep/identity";
+import { TaggedErrorClass } from "@beep/schema";
+import { HttpStatus2XX, HttpStatus4XX, HttpStatus5XX } from "@beep/schema/HttpStatus";
 import * as S from "effect/Schema";
-import {SearchBody} from "../..//values/index.ts";
-import {SearchResponse} from "../../values/SearchResponse/index.ts";
-import {HttpApiSchema} from "effect/unstable/httpapi";
-import {HttpStatus2XX, HttpStatus4XX, HttpStatus5XX} from "@beep/schema/HttpStatus";
+import { HttpApiSchema } from "effect/unstable/httpapi";
+import { SearchBody } from "../..//values/index.ts";
+import { SearchResponse } from "../../values/SearchResponse/index.ts";
 
 const $I = $GovinfoId.create("domain/contracts/Search/Search.contract");
 
@@ -24,10 +24,12 @@ const $I = $GovinfoId.create("domain/contracts/Search/Search.contract");
  * @category models
  * @since 0.0.0
  */
-export class Payload extends SearchBody.extend<Payload>($I`Payload`)({}, $I.annote("Payload", {
-	description: "",
-})) {
-}
+export class Payload extends SearchBody.extend<Payload>($I`Payload`)(
+  {},
+  $I.annote("Payload", {
+    description: "",
+  })
+) {}
 
 /**
  * TODO
@@ -39,56 +41,62 @@ export class Payload extends SearchBody.extend<Payload>($I`Payload`)({}, $I.anno
  * @category models
  * @since 0.0.0
  */
-export class Success extends SearchResponse.extend<Success>($I`Success`)({}, $I.annote("Success", {
-	description: "",
-	status: HttpStatus2XX.From.Enum.Ok,
-})) {
-}
+export class Success extends SearchResponse.extend<Success>($I`Success`)(
+  {},
+  $I.annote("Success", {
+    description: "",
+    status: HttpStatus2XX.From.Enum.Ok,
+  })
+) {}
 
+export class FailureBadRequest extends TaggedErrorClass<FailureBadRequest>($I`FailureBadRequest`)(
+  "FailureBadRequest",
+  {
+    cause: S.OptionFromOptionalKey(S.Defect()),
+    status: S.tag(HttpStatus4XX.From.Enum.BadRequest),
+  },
+  $I.annote("FailureBadRequest", {
+    description: "",
+  })
+) {}
 
-export class FailureBadRequest extends TaggedErrorClass<FailureBadRequest>($I`FailureBadRequest`)("FailureBadRequest", {
-	cause: S.OptionFromOptionalKey(S.Defect()),
-	status: S.tag(HttpStatus4XX.From.Enum.BadRequest),
-}, $I.annote("FailureBadRequest", {
-	description: "",
-})) {
-}
+export class FailureNotFound extends TaggedErrorClass<FailureNotFound>($I`FailureNotFound`)(
+  "FailureNotFound",
+  {
+    cause: S.OptionFromOptionalKey(S.Defect()),
+    status: S.tag(HttpStatus4XX.From.Enum.NotFound),
+  },
+  $I.annote("FailureNotFound", {
+    description: "",
+  })
+) {}
 
-export class FailureNotFound extends TaggedErrorClass<FailureNotFound>($I`FailureNotFound`)("FailureNotFound", {
-	cause: S.OptionFromOptionalKey(S.Defect()),
-	status: S.tag(HttpStatus4XX.From.Enum.NotFound),
-}, $I.annote("FailureNotFound", {
-	description: "",
-})) {
-}
-
-export class FailureInternalServerError extends TaggedErrorClass<FailureInternalServerError>($I`FailureInternalServerError`)("FailureInternalServerError",
-	{
-		cause: S.OptionFromOptionalKey(S.Defect()),
-		status: S.tag(HttpStatus5XX.From.Enum.InternalServerError),
-	},
-	$I.annote("FailureInternalServerError", {
-		description: "",
-	}),
-) {
-}
+export class FailureInternalServerError extends TaggedErrorClass<FailureInternalServerError>(
+  $I`FailureInternalServerError`
+)(
+  "FailureInternalServerError",
+  {
+    cause: S.OptionFromOptionalKey(S.Defect()),
+    status: S.tag(HttpStatus5XX.From.Enum.InternalServerError),
+  },
+  $I.annote("FailureInternalServerError", {
+    description: "",
+  })
+) {}
 
 export const Failure = S.Union([
-	FailureBadRequest.pipe(
-		HttpApiSchema.status(400)
-	),
-	FailureNotFound.pipe(
-		HttpApiSchema.status(404)
-	),
-	FailureInternalServerError.pipe(
-		HttpApiSchema.status(500)
-	),
-]).pipe(S.toTaggedUnion("_tag"), $I.annoteSchema("Failure", {
-	description: ""
-}))
+  FailureBadRequest.pipe(HttpApiSchema.status(400)),
+  FailureNotFound.pipe(HttpApiSchema.status(404)),
+  FailureInternalServerError.pipe(HttpApiSchema.status(500)),
+]).pipe(
+  S.toTaggedUnion("_tag"),
+  $I.annoteSchema("Failure", {
+    description: "",
+  })
+);
 
 export type Failure = typeof Failure.Type;
 
 export declare namespace Failure {
-	export type Encoded = typeof Failure.Encoded;
+  export type Encoded = typeof Failure.Encoded;
 }
